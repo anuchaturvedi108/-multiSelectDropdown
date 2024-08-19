bfa2abaaa3f7b32b96adfec038316a115183d899
Anshu <86436793+anuchaturvedi108@users.noreply.github.com>
2024-08-19 21:42:18 +0530

    Update MultiSelectDropdown.jsx
    Updated the handleChange Logic

--- my-app/src/MultiSelectDropdown.jsx
+++ my-app/src/MultiSelectDropdown.jsx
@@ -5,24 +5,13 @@ const MultiSelectDropdown = ({
   selectedOptions,
   setSelectedOptions,
 }) => {
-  // For multiSelect
   const handleChange = (e) => {
-    const value = e.target.value;
-    if (selectedOptions.includes(value)) {
-      setSelectedOptions(selectedOptions.filter((item) => item !== value));
-    } else {
-      setSelectedOptions([...selectedOptions, value]);
-    }
+    const selectedValues = Array.from(
+      e.target.selectedOptions,
+      (option) => option.value
+    );
+    setSelectedOptions(selectedValues);
   };
-
-  // For singleSelect
-  // const handleChange = (e) => {
-  //   const selectedValues = Array.from(
-  //     e.target.selectedOptions,
-  //     (option) => option.value
-  //   );
-  //   setSelectedOptions(selectedValues);
-  // };
 
   return (
     <div>
